ecb6955b4ab1f68c64bf9b4831f2ad06dfd05016
akb <doge.wow.so.scare@gmail.com>
2020-04-22 11:01:48 +0900

    refactor: Separate Cypress test function

--- MyApp/cypress/integration/user_login.spec.js
+++ MyApp/cypress/integration/user_login.spec.js
@@ -3,14 +3,16 @@ const users = [
 ];
 
 describe("My First Test", function() {
-  it("Visit Vue.js frontend page!", function() {
+  it("Register a new member", function() {
     cy.visit("/");
     cy.get('input[name="user_name"]').type(users[0].name);
     cy.get('input[name="password"]').type(users[0].password);
     cy.get('input[value="male"]').check();
     cy.get('select[name="office_place"]').select(users[0].office);
     cy.get('input[id="registerButton"]').click();
+  });
 
+  it("Login as the newly registered member", function() {
     cy.visit("/login");
     // cy.get('input[name="user_name"]').type(users[0].name);
     cy.get('input[placeholder="ユーザー名"]').type(users[0].name);
