e6bbc3ea177e32c1c56d10791c4a853bd18c4e2b
akb <doge.wow.so.scare@gmail.com>
2020-04-22 11:52:09 +0900

    test(Cypress): Check alert box content on user register & user login

--- MyApp/cypress/integration/user_login.spec.js
+++ MyApp/cypress/integration/user_login.spec.js
@@ -1,22 +1,51 @@
-const users = [
-  { name: "織田信長", password: "oda1234", sex: "male", office: "大阪事業所" }
+const good_users = [
+  { name: "織田信長", password: "oda1234", sex: "male", office: "大阪事業所" },
+  {
+    name: "北条政子",
+    password: "gokamakura1234",
+    sex: "female",
+    office: "仙台事業所",
+  },
+  {
+    name: "太田道灌",
+    password: "edo1234",
+    sex: "male",
+    office: "札幌事業所",
+  },
 ];
 
-describe("My First Test", function() {
-  it("Register a new member", function() {
-    cy.visit("/");
-    cy.get('input[name="user_name"]').type(users[0].name);
-    cy.get('input[name="password"]').type(users[0].password);
-    cy.get('input[value="male"]').check();
-    cy.get('select[name="office_place"]').select(users[0].office);
-    cy.get('input[id="registerButton"]').click();
-  });
+describe("正常ログイン機能テスト", function() {
+  good_users.map((user) => {
+    it("ユーザー新規登録試行: " + user.name, function() {
+      cy.visit("/");
+      cy.get('input[name="user_name"]').type(user.name);
+      cy.get('input[name="password"]').type(user.password);
+      cy.get('input[value="male"]').check();
+      cy.get('select[name="office_place"]').select(user.office);
+      cy.get('input[id="registerButton"]').click();
 
-  it("Login as the newly registered member", function() {
-    cy.visit("/login");
-    // cy.get('input[name="user_name"]').type(users[0].name);
-    cy.get('input[placeholder="ユーザー名"]').type(users[0].name);
-    cy.get('input[name="password"]').type(users[0].password);
-    cy.get('input[id="loginButton"]').click();
+      const stub = cy.stub();
+      cy.on("window:alert", stub);
+      cy.get('input[id="registerButton"]')
+        .click()
+        .then(() => {
+          expect(stub.getCall(0)).to.be.calledWith("登録が完了しました");
+        });
+    });
+
+    it("登録済ユーザーログイン試行: " + user.name, function() {
+      cy.visit("/login");
+      cy.get('input[placeholder="ユーザー名"]').type(user.name);
+      cy.get('input[name="password"]').type(user.password);
+      cy.get('input[id="loginButton"]').click();
+
+      const stub = cy.stub();
+      cy.on("window:alert", stub);
+      cy.get('input[id="loginButton"]')
+        .click()
+        .then(() => {
+          expect(stub.getCall(0)).to.be.calledWith("ログインに成功しました");
+        });
+    });
   });
 });
