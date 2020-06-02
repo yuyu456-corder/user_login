354a24e2d7f09d328ecccc9e0e59d57ee24ef7bb
yuyu456 <youq27668@gmail.com>
2020-06-02 19:53:50 +0900

    fix: CypressをVuetify用に改変した

--- MyApp/cypress/integration/user_login.spec.js
+++ MyApp/cypress/integration/user_login.spec.js
@@ -8,54 +8,65 @@ const good_users = [
   },
 ];
 
+//正常系のログイン機能のテストコード
 describe("user_loginの正常ログイン機能をテストします！", function () {
   good_users.map((user) => {
     it("ユーザー新規登録試行: " + user.name, function () {
+
       cy.visit("/");
-      cy.get('input[name="user_name"]').type(user.name);
-      cy.get('input[name="password"]').type(user.password);
-      cy.get('input[name="sex"]').check(user.sex);
-      cy.get('select[name="office_place"]').select(user.office);
-      cy.get('input[id="registerButton"]').click();
+      cy.get('[data-cy=userName]').type(user.name);
+      cy.get('[data-cy=password]').type(user.password);
+      if (user.sex == "male") {
+        cy.get('[data-cy=sex_male]').check({ force: true });
+      } else {
+        cy.get('[data-cy=sex_female]').check({ force: true });
+      }
+      cy.get('[data-cy=officePlaces]').type(user.office + '{enter}', { force: true });
+      cy.get('[data-cy=registerButton]').click({ force: true });
 
-      cy.wait(200);
+      cy.contains("アカウント登録が完了しました");
 
-      const stub = cy.stub();
-      cy.on("window:alert", stub);
-      cy.get('input[id="registerButton"]').then(() => {
-        expect(stub.getCall(0)).to.be.calledWith("登録が完了しました");
-      });
+      // cy.get('[data-cy=registerButton]').then(() => {
+      //     expect(stub.getCall(0)).to.be.calledWith("登録が完了しました");
+      // });
     });
 
     it("登録済ユーザーログイン試行: " + user.name, function () {
       cy.visit("/login");
-      cy.get('input[placeholder="ユーザー名"]').type(user.name);
-      cy.get('input[name="password"]').type(user.password);
-      cy.get('input[id="loginButton"]').click();
+      cy.get('[data-cy=userName]').type(user.name);
+      cy.get('[data-cy=password]').type(user.password);
+      cy.get('[data-cy=loginButton]').click();
 
-      const stub = cy.stub();
-      cy.on("window:alert", stub);
-      cy.get('input[id="loginButton"]')
-        .click()
-        .then(() => {
-          expect(stub.getCall(0)).to.be.calledWith("ログインに成功しました");
-        });
+      //ログインに成功するとマイページに遷移するためそのページのテキストノードを検知する
+      cy.contains("This Page is Your MyPage");
+
+      // const stub = cy.stub();
+      // cy.on("window:alert", stub);
+      // cy.get('[data-cy=loginButton]')
+      //   .click()
+      //   .then(() => {
+      //     expect(stub.getCall(0)).to.be.calledWith("ログインに成功しました");
+      //   });
     });
 
     it("ユーザーマスタでの存在確認: " + user.name, function () {
       cy.visit("/AdministratorPage");
       cy.wait(200);
-      cy.get("tr")
+      cy.get('[data-cy=accountTable]')
         .eq(-1)
         .should("contain", user.name);
     });
 
     it("ユーザーマスタからの削除テスト: " + user.name, function () {
+      //対象ユーザのチェックボックスを選択
       cy.visit("/AdministratorPage");
       cy.wait(200);
-      cy.get("tr>td>button")
-        .eq(-1)
-        .click();
+      cy.get('i')
+        //テーブル下のアイコンもi要素のためそれを取り除いている
+        .eq(-4)
+        .click({ force: true });
+      //削除ボタンをクリック
+      cy.get('[data-cy=removeAccount]').click({ force: true })
     });
   });
 });
